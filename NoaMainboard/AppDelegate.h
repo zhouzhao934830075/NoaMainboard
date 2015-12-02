41df8f2f191c72731a529f92f53dd73a548abf94
zhouzhao934830075 <jordan@halos-imac.local>
2015-12-02 14:30:11 +0800

    Initial Commit

---
+++ NoaMainboard/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  NoaMainboard
+//
+//  Created by Jordan Zhou on 15/12/2.
+//  Copyright © 2015年 Jordan Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ NoaMainboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NoaMainboard
+//
+//  Created by Jordan Zhou on 15/12/2.
+//  Copyright © 2015年 Jordan Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
